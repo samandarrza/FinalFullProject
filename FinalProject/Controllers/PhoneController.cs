91a031969e985409b4807861e7c6b71d7ae4c9e3
samandarrza <samandarshr@code.edu.az>
2023-03-06 19:30:18 +0400

    add modal partial

---
+++ FinalProject/Controllers/PhoneController.cs
@@ -0,0 +1,22 @@
+﻿using FinalProject.DAL;
+using FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Controllers
+{
+    public class PhoneController : Controller
+    {
+        private readonly EtradeDbContext _context;
+        public PhoneController(EtradeDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult GetPhone(int id)
+        {
+            Phone phone = _context.Phones.Include(x=>x.PhoneImages).Include(x=>x.Memory).Include(x=>x.PhoneModel).FirstOrDefault(x=>x.Id == id);
+
+            return PartialView("_PhoneModelPartial", phone);
+        }
+    }
+}
---
+++ FinalProject/wwwroot/assets/js/app.js
@@ -0,0 +1,10 @@
+﻿$(document).on("click", ".phone-modal-btn", function (e) {
+    e.preventDefault()
+    let url = $(this).attr("href");
+    fetch(url).then(response => response.text())
+        .then(data => {
+            $("#quick-view-modal .modal-dialog").html(data)
+        })
+    $("#quick-view-modal").modal("show")
+    
+})
